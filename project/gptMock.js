7a556ae26fb08c2806c220efa95d5a35c800d71e
blinkert <ligma4003@gmail.com>
2025-05-18 17:51:22 +0300

    Update gptMock.js

--- project/gptMock.js
+++ project/gptMock.js
@@ -1,5 +1,5 @@
 function isOffensive(message ) {
-    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "fuckyou","faggot","kill yout self","kys"];
+    const badWords = ["טיפש", "אידיוט", "מטומטם", "fuck", "fuckyou","faggot","kill yout self","kys","stupid","i hate trump","joe biden is good","i love russia","idiot","מניאק"];
     return badWords.some(word => message.includes(word));
   }
   
