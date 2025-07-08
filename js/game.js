03da58354a4c5b9175ae4d65d4cd1c0c4e3729d0
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2025-07-08 12:22:25 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -53,35 +53,35 @@ if (currentPlayer && currentPlayer.reputation !== undefined) {
   const rep = currentPlayer.reputation;
 
   if (rep >= 9) {
-    p1Health = 100;
-    powerCooldown = 5000;
-    p1BodyHeight = 100;
-    p1BodyWidth = 100;
+    p2Health = 100;
+    fastCooldown = 5000;
+    p2BodyHeight = 100;
+    p2BodyWidth = 100;
   } else if (rep >= 8) {
-    p1Health = 90;
-    powerCooldown = 6000;
-    p1BodyHeight = 110;
-    p1BodyWidth = 110;
+    p2Health = 90;
+    fastCooldown = 6000;
+    p2BodyHeight = 110;
+    p2BodyWidth = 110;
   } else if (rep >= 6) {
-    p1Health = 80;
-    powerCooldown = 7000;
-    p1BodyHeight = 125;
-    p1BodyWidth = 125;
+    p2Health = 80;
+    fastCooldown = 7000;
+    p2BodyHeight = 125;
+    p2BodyWidth = 125;
   } else if (rep >= 4) {
-    p1Health = 60;
-    powerCooldown = 8000;
-    p1BodyHeight = 150;
-    p1BodyWidth = 150;
+    p2Health = 60;
+    fastCooldown = 8000;
+    p2BodyHeight = 150;
+    p2BodyWidth = 150;
   } else if (rep >= 2) {
-    p1Health = 40;
-    powerCooldown = 9000;
-    p1BodyHeight = 175;
-    p1BodyWidth = 175;
+    p2Health = 40;
+    fastCooldown = 9000;
+    p2BodyHeight = 175;
+    p2BodyWidth = 175;
   } else {
-    p1Health = 20;
-    powerCooldown = 10000;
-    p1BodyHeight = 200;
-    p1BodyWidth = 200;
+    p2Health = 20;
+    fastCooldown = 10000;
+    p2BodyHeight = 200;
+    p2BodyWidth = 200;
   }
 }
 
@@ -261,7 +261,7 @@ function showWinner() {
   ctx.fillStyle = "white";
   ctx.font = "50px Arial";
   ctx.textAlign = "center";
-  ctx.fillText(`${winner} has won!`, canvas.width / 2, canvas.height / 2);
+  ctx.fillText((winner == "Player 2")? "you won!" : "you lose :(", canvas.width / 2, canvas.height / 2);
 }
 
 function gameLoop() {
